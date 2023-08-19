8e4fcaa70e0f4ba18054bbf87220a2bfe25f44bc
stormymayday <stormymayday7@gmail.com>
2023-08-19 16:08:40 -0700

    Destructuring the img, title, and author props

--- src/index.js
+++ src/index.js
@@ -34,16 +34,18 @@ const BookList = () => {
 
 };
 
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
 
-  console.log(props);
+  // console.log(props);
+
+  // const { img, title, author } = props;
 
   return (
 
     <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
 
   );

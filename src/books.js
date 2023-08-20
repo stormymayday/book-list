2737ec2fe0c6fcb95788473756744bbfbd8a424b
stormymayday <stormymayday7@gmail.com>
2023-08-20 16:04:43 -0700

    Moved images to the src folder

--- src/books.js
+++ src/books.js
@@ -1,22 +1,26 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpeg';
+import img3 from './images/book-3.jpeg';
+
 const books = [
 
     {
         id: 1,
-        img: `./images/book-1.jpg`,
+        img: img1,
         title: `Clean Code: A Handbook of Agile Software Craftsmanship`,
         author: `Robert Martin`,
 
     },
     {
         id: 2,
-        img: `./images/book-2.jpeg`,
+        img: img2,
         title: `Domain-Driven Design: Tackling Complexity in the Heart of Software`,
         author: `Timo Keni`,
 
     },
     {
         id: 3,
-        img: `./images/book-3.jpeg`,
+        img: img3,
         title: `How to Stop Worrying and Start Living`,
         author: `Dale Carnegie`,
 

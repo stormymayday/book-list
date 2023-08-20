e069217491d0b0dd1eea3eaef6f9b433fc941206
stormymayday <stormymayday7@gmail.com>
2023-08-20 15:41:31 -0700

    Moved the books array into it's own module

---
+++ src/books.js
@@ -0,0 +1,20 @@
+const books = [
+
+    {
+        id: 1,
+        img: `./images/book-1.jpg`,
+        title: `Clean Code: A Handbook of Agile Software Craftsmanship`,
+        author: `Robert Martin`,
+
+    },
+    {
+        id: 2,
+        img: `./images/book-2.jpeg`,
+        title: `Domain-Driven Design: Tackling Complexity in the Heart of Software`,
+        author: `Timo Keni`,
+
+    },
+
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
-
-const books = [
-
-  {
-    id: 1,
-    img: `./images/book-1.jpg`,
-    title: `Clean Code: A Handbook of Agile Software Craftsmanship`,
-    author: `Robert Martin`,
-
-  },
-  {
-    id: 2,
-    img: `./images/book-2.jpeg`,
-    title: `Domain-Driven Design: Tackling Complexity in the Heart of Software`,
-    author: `Timo Keni`,
-
-  },
-
-];
+import books from './books.js';
 
 const BookList = () => {
 
   return (
 
-    <section className='booklist'>
+    <>
 
-      {books.map((book) => {
+      <section className='booklist'>
 
-        return (
-          <Book key={book.id} {...book} />
-        );
+        {books.map((book) => {
 
-      })}
+          return (
+            <Book key={book.id} {...book} />
+          );
 
-    </section>
+        })}
 
-  )
+      </section>
+
+    </>
+
+  );
 
 };
 
 const Book = (props) => {
 
-  console.log(props);
+  // console.log(props);
 
   const { img, title, author } = props;
 
